1f761eeb50a79d3d70bc2fc390a950424693a518
Kittipat Poosrimoung <first1801@hotmail.com>
2021-04-14 00:12:39 +0700

    connect mongodb

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const config = require("config");
-
+const mongoose = require("mongoose");
 const app = express();
 
+mongoose
+  .connect("mongodb://localhost/Ocsv", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB..."));
+
+app.use("/api/v1/users/register");
 app.use("/", (req, res) => {
   res.status(200).send("Hello");
 });
