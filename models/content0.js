fcb0a9b02fadfabfe96bd4407338d3986a11864e
Kittipat Poosrimoung <first1801@hotmail.com>
2021-04-14 18:32:50 +0700

    change design of content

--- models/content0.js
+++ models/content0.js
@@ -1,9 +1,35 @@
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
+const { mainSchema, mainValidateSchema } = require("./mainTopic");
+const { subSchema, subValidateSchema } = require("./subTopic");
 
+// const mainSchema = new mongoose.Schema({
+//   courseID: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     required: true,
+//   },
+//   title: {
+//     type: String,
+//     required: true,
+//     maxlength: 255,
+//   },
+// });
+// const subSchema = new mongoose.Schema({
+//   main: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     required: true,
+//   },
+//   title: {
+//     type: String,
+//     required: true,
+//     maxlength: 255,
+//   },
+// });
 
 const contentSchema = new mongoose.Schema({
+  mainTopic: [mainSchema],
+  subTopic: [subSchema],
   content: {
     type: String,
   },
@@ -32,8 +58,18 @@ const contentSchema = new mongoose.Schema({
 const Content = mongoose.model("Content", contentSchema);
 
 function validateContent(content) {
+  //   const myMainSchema = Joi.object({
+  //     courseID: Joi.objectId().required(),
+  //     title: Joi.string().max(255).required(),
+  //   });
+  //   const mySubSchema = Joi.object({
+  //     main: Joi.objectId().required(),
+  //     title: Joi.string().max(255).required(),
+  //   });
 
   const schema = Joi.object({
+    mainTopic: Joi.array().items(mainValidateSchema),
+    subTopic: Joi.array().items(subValidateSchema),
     content: Joi.string(),
     sub: Joi.objectId(),
     contentType: Joi.string().max(32),
