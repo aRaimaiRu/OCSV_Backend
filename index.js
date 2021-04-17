3372316fb41ecb444054563836ef6574f572e681
Kittipat Poosrimoung <first1801@hotmail.com>
2021-04-17 18:54:24 +0700

    cors and json

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const login = require("./routes/auth");
 const course = require("./routes/course");
 const allcontent = require("./routes/allcontent");
 const app = express();
-
+const cors = require("cors");
 mongoose
   .connect("mongodb://localhost/Ocsv", {
     useNewUrlParser: true,
@@ -17,13 +17,17 @@ mongoose
   .catch((err) => console.error("Could not connect to MongoDB..."));
 
 app.use(express.json());
+app.use(cors());
+app.use("/", (req, res, next) => {
+  console.log(req);
+  // res.status(200).send("Hello");
+  next();
+});
+
 app.use("/api/v1/users", users); //register
 app.use("/api/v1/auth", login);
 app.use("/api/v1/courses", course);
 app.use("/api/v1/allcontent", allcontent);
-app.use("/", (req, res) => {
-  res.status(200).send("Hello");
-});
 
 const port = config.get("port") || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
