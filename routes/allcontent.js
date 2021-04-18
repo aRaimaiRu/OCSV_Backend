cf9cc2d5d84ffc2df9600397a44d61ad84011511
Kittipat Poosrimoung <first1801@hotmail.com>
2021-04-18 16:25:25 +0700

    validate _id

--- routes/allcontent.js
+++ routes/allcontent.js
@@ -7,7 +7,7 @@ const { AllContent, validate } = require("../models/allcontent");
 const { Main } = require("../models/mainTopic");
 const { Sub } = require("../models/subTopic");
 const { Content } = require("../models/content");
-
+const { validate_id } = require("../models/validate_id");
 router.get("/", auth, async (req, res) => {
   let allcontent = await AllContent.find({ AuthorID: req.user._id });
   res.status(200).send(allcontent);
@@ -61,6 +61,68 @@ router.post("/create", auth, async (req, res) => {
   });
   allcontent.save();
   res.status(200).send(allcontent);
+});
+
+router.put("/edit", auth, async (req, res) => {
+  try {
+    req.body.mainTopic
+      ? req.body.mainTopic.map((c) => new Main(c))
+      : req.body.mainTopic;
+    req.body.subTopic
+      ? req.body.subTopic.map((c) => new Sub(c))
+      : req.body.subTopic;
+    req.body.content
+      ? req.body.content.map((c) => new Content(c))
+      : req.body.content;
+
+    console.log("req = ", req.body);
+    const { _id, ...bodyNoID } = req.body;
+    const { id_error } = validate_id({ _id });
+    if (id_error) return res.status(400).send(error.details[0].message);
+    const { error } = validate(bodyNoID);
+    if (error) return res.status(400).send(error.details[0].message);
+  } catch (e) {
+    console.log("My error", e);
+  }
+  // const { _id, module, grade, mainTopic, subTopic, content } = req.body;
+  // let myAllContent = await AllContent.findById(_id);
+  // myAllContent.modlue = module;
+  // myAllContent.grade = grade;
+  // ///maintopic
+  // myAllContent.mainTopic.title = mainTopic.title;
+  // myAllContent.mainTopic.id = mainTopic.id;
+  // ////////subtopic
+  // myAllContent.subTopic.title = subTopic.title;
+  // myAllContent.subTopic.id = subTopic.id;
+  // myAllContent.subTopic.main = subTopic.main;
+  // ///content
+  // myAllContent.content.content = content.content;
+  // myAllContent.content.id = content.id;
+  // myAllContent.content.sub = content.sub;
+  // myAllContent.content.contentType = content.contentType;
+  // myAllContent.content.Explain = content.Explain;
+  // myAllContent.content.outLink = content.outLink;
+  // myAllContent.content.Answer = content.Answer;
+  // myAllContent.content.Choice = content.Choice;
+  // myAllContent.content.Picture = content.Picture;
+
+  // myAllContent.save();
+  // res.status(200).send(myAllContent);
+
+  let myAllContent = await AllContent.replaceOne({ _id }, req.body);
+  res.status(200).json(req.body);
+});
+
+router.delete("/delete", auth, async (req, res) => {
+  console.log("req = ", req.body);
+
+  const { _id, ...bodyNoID } = req.body;
+  const { id_error } = validate_id({ _id });
+  if (id_error) return res.status(400).send(error.details[0].message);
+  let myAllContent = await AllContent.findByIdAndRemove(_id, {
+    useFindAndModify: false,
+  });
+  res.status(200).json(myAllContent);
 });
 
 module.exports = router;
