3997c5bed8f39313e28b7642396d47198cb6d603
Kittipat Poosrimoung <first1801@hotmail.com>
2021-04-18 16:25:05 +0700

    change json

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/me", auth, async (req, res) => {
   const user = await User.findById(req.user._id).select("-password");
-  res.send(user);
+  res.json(user);
 });
 
 router.post("/register", async (req, res) => {
@@ -16,14 +16,14 @@ router.post("/register", async (req, res) => {
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
-  
+
   user = new User(_.pick(req.body, ["email", "password"]));
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   const token = user.generateAuthToken();
-  res.header("x-auth-token", token).send(_.pick(user, ["_id", "email"]));
+  res.header("x-auth-token", token).json(_.pick(user, ["_id", "email"]));
 });
 
 module.exports = router;
