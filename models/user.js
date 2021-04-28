52b7b43c5fdd5da0e9401ec40bd7cb589894ddac
Kittipat Poosrimoung <first1801@hotmail.com>
2021-04-29 00:07:54 +0700

    set jwt expires

--- models/user.js
+++ models/user.js
@@ -20,7 +20,9 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"), {
+    expiresIn: "7d", // expires in 7 days
+  });
   return token;
 };
 
